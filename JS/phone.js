4cbe0939086b146a4af8bf425c79d11e8bd15ac6
bariulmunshi <bariulsoft@gmail.com>
2023-10-20 22:13:33 +0600

    added

--- JS/phone.js
+++ JS/phone.js
@@ -7,16 +7,16 @@ Using async/await with fetch is a way to work with asynchronous code in a more s
 In your code example, using async/await with fetch simplifies the process of making an HTTP request and handling the response, making your code more maintainable and easier to work wit
 */
 
-const loadPhone = async (searchText) => {
+const loadPhone = async (searchText='13',isShowAll) => {
   const res = await fetch(
     `https://openapi.programming-hero.com/api/phones?search=${searchText}`
   );
   const data = await res.json();
   const phones = data.data;
-  displayPhones(phones);
+  displayPhones(phones,isShowAll);
 };
 
-const displayPhones = (phones) => {
+const displayPhones = (phones,isShowAll) => {
   // console.log(phones);
 
   const phoneContainer = document.getElementById("phone-container");
@@ -24,17 +24,22 @@ const displayPhones = (phones) => {
 
     //display all show button if there are more than 6 phones
     const showAllContainer=document.getElementById("show-all-container");
-    if(phones.length >12){
+    if(phones.length >12 && !isShowAll){
             showAllContainer.classList.remove('hidden');
     }
     else{
             showAllContainer.classList.add('hidden');
     }
-    //display only first 12  phones
-    phones= phones.slice(0,12);  
+
+    // console.log('Is Show All',isShowAll);
+    //display only first 12  phones if not show-all
+
+    if(!isShowAll)  {
+      phones= phones.slice(0,12);
+    }
     
     phones.map((phone) => {
-    console.log(phone);
+    // console.log(phone);
     //step-1: create a div
     const phoneCard = document.createElement("div");
     phoneCard.classList = `card p-4 bg-gray-100 shadow-xl`;
@@ -43,8 +48,8 @@ const displayPhones = (phones) => {
         <div class="card-body">
             <h2 class="card-title">${phone.phone_name}</h2>
             <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div class="card-actions justify-end">
-            <button class="btn btn-primary">Buy Now</button>
+            <div class="card-actions justify-center">
+            <button onclick="handleShowDetails('${phone.slug}')" class="btn btn-primary">Show Details</button>
             </div>
         </div>
         `;
@@ -54,15 +59,40 @@ const displayPhones = (phones) => {
   toggleLoadingSpinner(false);
 };
 
+const handleShowDetails = async(id) => {
+  // console.log('Show Details',id);
+  /* Load single phone data */
+  const res=await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+  const data=await res.json();
+  const phone=data.data;
+  showPhoneDetails(phone);
+}
+
+const showPhoneDetails=(phone)=>{
+  console.log(phone);
+  const phoneName=document.getElementById('show-details-phone-name');
+  phoneName.innerText=phone.name;
+
+  const showDetailContainer=document.getElementById('show-detail-container');
+ 
+    showDetailContainer.innerHTML = `
+       <img src="${phone.image}"/>
+       <p> <span> Storage:</span>${phone?.mainFeatures?.storage} </p>  
+       <p> <span> Display:</span>${phone?.mainFeatures?.displaySize} </p>  
+       <p> <span> GPS:</span>${phone?.others?.GPS || 'No GPS'} </p>  
+        `;
+  //show the modal
+  show_details_modal.showModal();
+}
 /*
 handle search button click 
  */
-const handleSearch = () => {
+const handleSearch = (isShowAll) => {
   toggleLoadingSpinner(true);
   const searchField = document.getElementById("search-field");
   const searchText = searchField.value;
   console.log(searchText);
-  loadPhone(searchText);
+  loadPhone(searchText,isShowAll);
 };
 
 const toggleLoadingSpinner=(isLoading) => {
@@ -77,6 +107,6 @@ const toggleLoadingSpinner=(isLoading) => {
 
 //handle show all
 const handleShowAll=() => {
-  
+  handleSearch(true);
 }
-// loadPhone();
+loadPhone();
--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Table of Contents
 
 ## step by step workFlow:
-1. 
+1. Data load using async await then show data in another function & clear all data after search each time
+2. if much data then show all button 
+3. spinner
+4. 
